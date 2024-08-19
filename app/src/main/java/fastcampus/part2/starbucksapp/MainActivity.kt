e06a2e47e9aa61e1b196632e1cd643ae816d6b06
HunCY5 <90296157+HunCY5@users.noreply.github.com>
2024-08-19 14:32:15 +0900

    Create Project

---
+++ app/src/main/java/fastcampus/part2/starbucksapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package fastcampus.part2.starbucksapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+    }
+}
